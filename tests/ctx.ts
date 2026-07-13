cc89a42e72da8aa3e3de1d242bf0dbf2b5b31831
Falldot <fe.offep@gmail.com>
2026-07-13 17:50:26 +1100

    test: session context

---
+++ tests/ctx.ts
@@ -0,0 +1,34 @@
+import { beforeEach, afterEach } from "bun:test";
+
+import { Client } from "../src";
+
+export interface Context {
+  client: Client;
+  session_id: string;
+  debug_pipe_name: string;
+}
+
+export function context() {
+  let ctx: Context = {} as Context;
+
+  beforeEach(async () => {
+    const url =
+      Bun.env["AS2MCA_API_URL"] ?? "http://localhost:3000/platform2mca/";
+    const username = Bun.env["AS2MCA_API_USERNAME"] ?? "test";
+    const password = Bun.env["AS2MCA_API_PASSWORD"] ?? "test";
+    
+    const client = new Client(url);
+    await client.authbasic(username, password);
+    const { session_id, debug_pipe_name } = await client.session_init();
+
+    ctx.client = client;
+    ctx.session_id = session_id;
+    ctx.debug_pipe_name = debug_pipe_name;
+  });
+
+  afterEach(async () => {
+    await ctx.client.session_deinit(ctx.session_id);
+  });
+
+  return ctx;
+}
---
+++ tests/session.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "bun:test";
+import { context } from "./ctx";
+
+describe("Session", () => {
+  const ctx = context();
+
+  test("`session_id` and `debug_pipe_name`", () => {
+    const { debug_pipe_name, session_id } = ctx;
+    
+    expect(session_id).toBeDefined();
+    expect(session_id).toMatch(/^[0-9a-fA-F]{32}$/);
+    
+    expect(debug_pipe_name).toBeDefined();
+    expect(debug_pipe_name).toMatch(/^debug\$\d{10}$/);
+  })
+});
